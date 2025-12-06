2b2a4580015a7d372fab3e099440cd5e8d220222
web admin <heitorgiacomini@gmail.com>
2025-12-06 18:28:17 -0300

    cho satanas

--- Shared/DDD/Aggregate.cs
+++ Shared/DDD/Aggregate.cs
@@ -1,16 +1,20 @@
-﻿
-
-namespace Shared.DDD
+﻿namespace Shared.DDD
 {
-    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId> where TId : class
-    {
-        private readonly List<IDomainEvent> _domainEvents => [];
-        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+	public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId> where TId : class
+	{
+		private readonly List<IDomainEvent> _domainEvents = [];
 
+		public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
-        public IDomainEvent[] ClearDomainEvents()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void AddDomainEvent(IDomainEvent domainEvent)
+		{
+			_domainEvents.Add(domainEvent);
+		}
+		public IDomainEvent[] ClearDomainEvents()
+		{
+			IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+			_domainEvents.Clear();
+			return dequeuedEvents;
+		}
+	}
 }
--- Shared/DDD/Entity.cs
+++ Shared/DDD/Entity.cs
@@ -1,12 +1,12 @@
 ﻿
 namespace Shared.DDD
 {
-    public abstract class Entity<T> : IEntity<T> where T : class
-    {
-        public required T Id { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public string? CreatedBy { get; set; }
-        public DateTime? LastModified { get; set; }
-        public string? LastModifiedBy { get; set; }
-    }
+	public abstract class Entity<T> : IEntity<T> where T : class
+	{
+		public required T Id { get; set; }
+		public DateTime? CreatedAt { get; set; }
+		public string? CreatedBy { get; set; }
+		public DateTime? LastModified { get; set; }
+		public string? LastModifiedBy { get; set; }
+	}
 }
