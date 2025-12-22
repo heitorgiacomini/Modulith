d6bb8fd42937568735d78ea94911e97d2dddd234
web admin <heitorgiacomini@gmail.com>
2025-12-22 02:31:10 -0300

    Handle product price updates across Catalog and Basket
    Implement integration event flow for product price changes:
    - Catalog now publishes ProductPriceChangedIntegrationEvent via MassTransit when a product's price is updated.
    - Basket service consumes this event and updates relevant basket item prices using a new command and handler.
    - Added validation for update commands.
    - Ensures basket prices stay in sync with catalog changes.

--- Modules/Catalog/Catalog/Products/Events/EventHandlers/ProductPriceChangedEventHandler.cs
+++ Modules/Catalog/Catalog/Products/Events/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -1,14 +1,31 @@
-﻿namespace Catalog.Products.Events.EventHandlers;
+using MassTransit;
+using Shared.Messaging.Events;
 
-public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
-	: INotificationHandler<ProductPriceChangedEvent>
+namespace Catalog.Products.Events.EventHandlers;
+
+public class ProductPriceChangedEventHandler
+  (IBus bus, ILogger<ProductPriceChangedEventHandler> logger)
+    : INotificationHandler<ProductPriceChangedEvent>
 {
 
-	public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
-	{
-		logger.LogInformation(
-			"Domain Event handled: {DomainEvent}", notification.GetType().Name);
+  public async Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+  {
+    logger.LogInformation(
+        "Domain Event handled: {DomainEvent}", notification.GetType().Name);
 
-		return Task.CompletedTask;
-	}
+    //return Task.CompletedTask;
+
+    // Publish product price changed integration event for update basket prices
+    ProductPriceChangedIntegrationEvent integrationEvent = new ProductPriceChangedIntegrationEvent
+    {
+      ProductId = notification.Product.Id,
+      Name = notification.Product.Name,
+      Category = notification.Product.Category,
+      Description = notification.Product.Description,
+      ImageFile = notification.Product.ImageFile,
+      Price = notification.Product.Price //set updated product price
+    };
+
+    await bus.Publish(integrationEvent, cancellationToken);
+  }
 }
