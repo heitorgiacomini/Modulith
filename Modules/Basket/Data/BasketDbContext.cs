58fa2dcd32ce4bbb9b4af3fa3723ac1c3c123bdf
web admin <heitorgiacomini@gmail.com>
2025-12-24 00:51:45 -0300

    Implement transactional outbox pattern in Basket service
    Introduce OutboxMessage entity and OutboxMessages table with new migration. Add OutboxProcessor background service to publish events reliably via MassTransit. Refactor checkout basket flow to write integration events to the outbox within a transaction. Add new endpoint and handler for basket checkout. Remove old migrations and update DbContext/model snapshot. Add BasketCheckoutIntegrationEvent and update project structure. Enables reliable, eventual-consistent event publishing.

--- Modules/Basket/Data/BasketDbContext.cs
+++ Modules/Basket/Data/BasketDbContext.cs
@@ -1,18 +1,18 @@
-﻿namespace Basket.Data;
+namespace Basket.Data;
 
 public class BasketDbContext : DbContext
 {
-    public BasketDbContext(DbContextOptions<BasketDbContext> options)
-        : base(options) { }
+  public BasketDbContext(DbContextOptions<BasketDbContext> options)
+      : base(options) { }
 
-    public DbSet<ShoppingCart> ShoppingCarts => this.Set<ShoppingCart>();
-    public DbSet<ShoppingCartItem> ShoppingCartItems => this.Set<ShoppingCartItem>();
-    //public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
+  public DbSet<ShoppingCart> ShoppingCarts => this.Set<ShoppingCart>();
+  public DbSet<ShoppingCartItem> ShoppingCartItems => this.Set<ShoppingCartItem>();
+  public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        _ = builder.HasDefaultSchema("basket");
-        _ = builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        base.OnModelCreating(builder);
-    }
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    _ = builder.HasDefaultSchema("basket");
+    _ = builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    base.OnModelCreating(builder);
+  }
 }
