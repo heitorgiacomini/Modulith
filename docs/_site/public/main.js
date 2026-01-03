b64a2f799d45dbb39958779ee581eb35a1d17e42
Heitor Giacomini <heitorgiacomini@gmail.com>
2026-01-02 21:10:14 -0300

    Add PaginationRequest class and related documentation
    - Introduced the PaginationRequest class in the Shared.Pagination namespace, implementing IEquatable for equality comparison.
    - Added properties PageIndex and PageSize with appropriate accessors.
    - Created YAML documentation for PaginationRequest, including constructor and property details.
    - Updated Shared.Pagination namespace documentation to include the new PaginationRequest class.
    - Modified table of contents to reflect the addition of PaginationRequest and its related items.

---
+++ docs/_site/public/main.js
@@ -0,0 +1,19 @@
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/heitorgiacomini',
+      title: 'GitHub'
+    },
+    {
+      icon: 'linkedin',
+      href: 'https://www.linkedin.com/in/heitor-giacomini/',
+      title: 'Twitter'
+    },
+    {
+      icon: 'heart',
+      href: 'https://github.com/heitorgiacomini/',
+      title: 'Sponsor'
+    }
+  ]
+}
